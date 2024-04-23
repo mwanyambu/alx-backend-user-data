62cf6ff74240504893689e555e1349c09234415b
mwanyambu <jmwanyambu@gmail.com>
2024-04-23 23:23:21 +0300

    pycode

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,13 +25,13 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ user_id by session """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
-    
+
     def current_user(self, request=None):
         """ user instance based on cookie value """
         session_cookie = self.session_cookie(request)
@@ -42,4 +42,3 @@ class SessionAuth(Auth):
             return None
         user = User.get(user_id)
         return user
-  
