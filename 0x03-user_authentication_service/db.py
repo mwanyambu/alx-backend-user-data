b4fe79f9e0d35d9ff0caa436044f86877eddb618
mwanyambu <jmwanyambu@gmail.com>
2024-04-27 14:29:54 +0300

    user_id

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,7 +51,7 @@ class DB:
         except InvalidRequestError as e:
             raise InvalidRequestError(f"Invalid query arguments: {e}")
 
-    def update_user(self, user_id: int, **kwargs):
+    def update_user(self, user_id: int, **kwargs) -> None:
         """ Update user """
         try:
             user = self.find_user_by(id=user_id)
